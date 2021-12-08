b93d3001253161fd1f39fe18312a1ab900b6ce60
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-07 20:52:50 -0500

    Primeros pasos usando exact

--- src/RouterV5.js
+++ src/RouterV5.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 export const RouterV5 = () => {
     return (
         <div>
-            <h1>RouterV5</h1>
+            <Router>
+                <Switch>
+                    <Route exact path='/'>
+                        <h3>Home</h3>
+                    </Route>
+                    <Route exact path='/acerca'>
+                        <h3>Acerca</h3>
+                    </Route>
+                    <Route exact path='/contacto'>
+                        <h3>contacto</h3>
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     );
 };
