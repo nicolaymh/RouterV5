28dad1c0c084410b92ac992dfdc259bfbd874e54
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-07 21:20:05 -0500

    Incorporando Links

--- src/RouterV5.js
+++ src/RouterV5.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export const RouterV5 = () => {
     return (
         <div>
             <Router>
+                <div className='container'>
+                    <nav className='navbar navbar-dark bg-dark'>
+                        <div className='container-fluid'>
+                            <Link className='navbar-brand' to='/'>
+                                Home
+                            </Link>
+                            <Link className='navbar-brand' to='/acerca'>
+                                Acerca
+                            </Link>
+                            <Link
+                                className='navbar-brand me-auto'
+                                to='/contacto'
+                            >
+                                Contacto
+                            </Link>
+                        </div>
+                    </nav>
+                </div>
+
                 <Switch>
                     <Route exact path='/'>
                         <h3>Home</h3>
