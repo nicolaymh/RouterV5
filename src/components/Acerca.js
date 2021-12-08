391bb4374b1017b8a1e7c755553566dccc792b4b
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-08 17:43:09 -0500

    Usando el hook useHistory

--- src/components/Acerca.js
+++ src/components/Acerca.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 
 export const Acerca = () => {
+    let history = useHistory();
+
+    const handleGoHome = () => {
+        console.log(history);
+
+        history.replace('/contacto');
+    };
+
     return (
         <div className='container'>
             <h3>Acerca</h3>
+
+            <button className='btn btn-outline-info' onClick={handleGoHome}>
+                Go Home
+            </button>
         </div>
     );
 };
