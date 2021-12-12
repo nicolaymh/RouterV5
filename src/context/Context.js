472c86625e6ee880f9bf479f5a602c758e8eb7f8
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-12 12:12:07 -0500

    instalando axios

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,11 +1,36 @@
-import React, { createContext, useEffect, useReducer } from 'react';
+import axios from 'axios';
+import React, { createContext, useEffect, useReducer, useState } from 'react';
 import { init } from '../helpers/init';
 import { userReducer } from './userReducer';
 
 const AuthContext = createContext();
 
+async function getProducts() {
+    try {
+        const response = await axios({
+            url: 'https://breakingbadapi.com/api/characters',
+            method: 'GET',
+        });
+
+        return response;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 const AuthProvider = ({ children }) => {
     const [user, dispatch] = useReducer(userReducer, {}, init);
+    // const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await getProducts();
+            console.log(response);
+            return response;
+        }
+
+        loadProducts();
+    });
 
     useEffect(() => {
         localStorage.setItem('user', JSON.stringify(user));
