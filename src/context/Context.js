75c0aa322e1020f2394c779e2f9821d41e3986b2
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-13 12:20:46 -0500

    Comentando el Context.js

--- src/context/Context.js
+++ src/context/Context.js
@@ -9,12 +9,15 @@ const AuthProvider = ({ children }) => {
     //* Reducer para manejar la autenticacion.
     const [user, dispatchUser] = useReducer(userReducer, {}, initUser);
 
+    //* useState que me guarda la data de la api REST Countries.
     const [continents, setContinents] = useState({ data: [], state: false });
 
+    //* UseEffect que me guarda si el usuario esta logged o no.
     useEffect(() => {
         localStorage.setItem('user', JSON.stringify(user));
     }, [user]);
 
+    //* useEffect que me consume la api de REST Countries solo al iniciar la aplicacion.
     useEffect(() => {
         async function loadContinents() {
             const response = await axiosGet();
