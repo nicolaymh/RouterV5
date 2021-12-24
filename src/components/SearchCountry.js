703bf830401f6df9f7ff0bcd84ae674442b688c0
Nicolay Moreno <nicolay.mherrera@gmail.com>
2021-12-24 10:05:48 -0500

    Eliminando mas archivos

--- src/components/SearchCountry.js
+++ src/components/SearchCountry.js
@@ -1,14 +1,7 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/Context';
-
 export const SearchCountry = () => {
-    const { dataCountry } = useContext(AuthContext);
-
     return (
         <div>
             <h3>Search Country</h3>
-
-            <h3>{dataCountry.estadoPeticion || 'esperando'}</h3>
         </div>
     );
 };
